59d4393c647c5a7b1ac443efc98f861106e9468a
vpg1 <malihtorovich@mail.ru>
2024-08-15 04:32:51 +0300

    UPD: Add some functions for working with files

---
+++ pkg/files_function/files_function.go
@@ -0,0 +1,33 @@
+package files_function
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func CheckIsFileExist(path string) bool {
+	// Checking is file exist
+	_, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return false
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	return true
+}
+
+func CreateFileIfNotExists(path string) error {
+	if !CheckIsFileExist(path) {
+		file, err := os.Create(path)
+		fmt.Println(file.Stat())
+		if err != nil {
+			log.Fatal("cannot create file: ", err)
+			return err
+		}
+	}
+
+	return nil
+}
