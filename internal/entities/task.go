dfd2a4ebfb3bff775dbfc25f97453f15cc5956dc
vpg1 <malihtorovich@mail.ru>
2024-08-15 03:07:56 +0300

    UPD: Add task object create function

--- internal/entities/task.go
+++ internal/entities/task.go
@@ -16,3 +16,7 @@ type Task struct {
 	createdAt   time.Time
 	completeAt  time.Time
 }
+
+func CreateTask(id int, name string, description string) *Task {
+	return &Task{id: id, name: name, description: description, status: NotDone, createdAt: time.Now(), completeAt: time.Time{}}
+}
