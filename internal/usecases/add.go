5c9cd7e58a0fa06b09fe0a7496a9d8c36e530267
vpg1 <malihtorovich@mail.ru>
2024-08-16 03:21:42 +0300

    FIX: Fix bug with incorrect issuance of IDs

---
+++ internal/usecases/add.go
@@ -0,0 +1,31 @@
+package usecases
+
+import (
+	"TaskTracker/internal/entities"
+	"fmt"
+	"log"
+)
+
+type TaskStorage interface {
+	SaveTask(e *entities.Task) error
+	GetTasks() ([]*entities.Task, error)
+	NumOfTasks() (int, error)
+	GetFreeId() (int, error)
+}
+
+func AddTask(storage TaskStorage, name string, description string) (int, error) {
+	freeId, err := storage.GetFreeId()
+	if err != nil {
+		return -1, err
+	}
+
+	newTask := entities.CreateTask(freeId, name, description)
+
+	if err := storage.SaveTask(newTask); err != nil {
+		return -1, fmt.Errorf("cant save task to storage")
+	}
+
+	log.Printf("Add task to storage: %s", newTask)
+
+	return freeId, nil
+}
