9b2ad8d52a779a31a132407b06674611a24adfb7
VPG1 <malihtorovich@mail.ru>
2024-08-19 19:42:38 +0300

    MINOR: Add String() method to Status struct

--- internal/entities/task.go
+++ internal/entities/task.go
@@ -4,6 +4,20 @@ import "time"
 
 type Status int
 
+func (s Status) String() string {
+	switch s {
+	case NotDone:
+		return "Not done"
+	case InProgress:
+		return "In progress"
+	case Done:
+		return "Done"
+	default:
+		panic("Not a valid status")
+
+	}
+}
+
 const (
 	NotDone = iota
 	InProgress
